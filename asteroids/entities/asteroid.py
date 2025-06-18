c200e56e6ff3cbb485bb85d56410bf614159ce68
Adam Bright <adam.brian.bright@gmail.com>
2025-06-19 00:50:35 +0400

    Added typing, TODOs, and a few small tweaks.

--- asteroids/entities/asteroid.py
+++ asteroids/entities/asteroid.py
@@ -1,17 +1,36 @@
-import pygame
 import random
-from asteroids.entities.circleshape import CircleShape
+
+import pygame
+
 from asteroids.constants import ASTEROID_MIN_RADIUS
+from .circleshape import CircleShape
+
+__all__ = ['Asteroid']
 
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius, *groups):
+    """
+    Asteroid entity.
+    """
+    __slots__ = ['velocity']
+
+    def __init__(self, x: float, y: float, radius: float, *groups: pygame.sprite.Group):
         super().__init__(x, y, radius, *groups)
+        self.velocity = pygame.Vector2(0, 0)
+        """Speed and direction of an asteroid"""
 
     def split(self):
+        """
+        Destroys the asteroid and creates asteroid shards.
+        TODO: Extract asteroid creation outside the asteroid entity class.
+        TODO: Asteroid should not know how to spawn another object.
+        TODO: Asteroid may contain information that would be helpful for Controller objects to reason about splitting.
+        :return:
+        """
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
+
         random_angle = random.uniform(20, 50)
         angle1 = self.velocity.rotate(random_angle)
         angle2 = self.velocity.rotate(-random_angle)
@@ -22,8 +41,18 @@ class Asteroid(CircleShape):
         asteroid1.velocity = angle1 * 1.2
         asteroid2.velocity = angle2 * 1.2
 
-    def draw(self, screen):
+    def draw(self, screen: pygame.Surface) -> None:
+        """
+        Draws the asteroid on the screen.
+        :param screen:
+        :return:
+        """
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
-    def update(self, dt):
-        self.position += self.velocity * dt
+    def update(self, delta_time: float) -> None:
+        """
+        Updates the asteroid.
+        :param delta_time:
+        :return:
+        """
+        self.position += self.velocity * delta_time
--- asteroids/entities/player.py
+++ asteroids/entities/player.py
@@ -1,6 +1,5 @@
 import pygame
-from asteroids.entities.circleshape import CircleShape
-from asteroids.entities.shot import Shot
+
 from asteroids.constants import (
     PLAYER_RADIUS,
     PLAYER_TURN_SPEED,
@@ -8,50 +7,97 @@ from asteroids.constants import (
     PLAYER_SHOOT_COOLDOWN,
     PLAYER_SPEED,
 )
+from .circleshape import CircleShape
+from .shot import Shot
+
+__all__ = ['Player']
 
 
 class Player(CircleShape):
-    def __init__(self, x, y, shot_groupe, *groups):
-        super().__init__(x, y, PLAYER_RADIUS, *groups)
-        self.rotation = 0
-        self.timer = 0
-        self.shot_groupe = shot_groupe
+    """
+    Player entity.
+    """
+    __slots__ = ['timer', 'shot_group']
 
-    # in the player class
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+    def __init__(self,
+                 x: float, y: float,
+                 shot_group: tuple[pygame.sprite.Group, ...],
+                 *groups: pygame.sprite.Group):
+        super().__init__(x, y, PLAYER_RADIUS, *groups)
+        self.timer: float = 0
+        """Player shooting cooldown timer"""
+        self.shot_group = shot_group
+        """Entity groups for bullet creation"""
+
+    def triangle(self) -> tuple[pygame.Vector2, pygame.Vector2, pygame.Vector2]:
+        """
+        Calculates vertices of the triangle shape representing the player.
+        :return:
+        """
+        forward = self.rotation
+        right = self.rotation.rotate(90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
-        return [a, b, c]
 
-    def draw(self, screen):
+        return a, b, c
+
+    def draw(self, screen: pygame.Surface) -> None:
+        """
+        Draws the player on the screen.
+        :param screen:
+        :return:
+        """
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+    def rotate(self, delta_time: float) -> None:
+        """
+        Rotates the player direction.
+        :param delta_time:
+        :return:
+        """
+        self.rotation = self.rotation.rotate(PLAYER_TURN_SPEED * delta_time)
 
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+    def move(self, delta_time: float) -> None:
+        """
+        Moves the player.
+        :param delta_time:
+        :return:
+        """
+        forward = self.rotation
+        self.position += forward * PLAYER_SPEED * delta_time
 
-    def shoot(self):
-        shot = Shot(self.position.x, self.position.y, self.shot_groupe)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    def shoot(self) -> None:
+        """
+        Shoots the bullet from the player.
 
-    def update(self, dt):
+        TODO: extract bullet creation from the player.
+        TODO: Player should only know about itself
+        TODO: Player may include reload timer and other shooting related info, but bullet creation must be outside.
+        :return:
+        """
+        shot = Shot(self.position.x, self.position.y, *self.shot_group)
+        shot.velocity = self.rotation * PLAYER_SHOOT_SPEED
+
+    def update(self, delta_time: float) -> None:
+        """
+        Updates the player.
+        :param delta_time:
+        :return:
+        """
         keys = pygame.key.get_pressed()
-        self.timer -= dt
+        self.timer -= delta_time
 
+        # TODO: Extract controls to a settings object (may be global variable).
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+            self.rotate(-delta_time)
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotate(delta_time)
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.move(delta_time)
         if keys[pygame.K_s]:
-            self.move(-dt)
+            self.move(-delta_time)
+        # TODO: Extract shooting logic outside the player class.
         if keys[pygame.K_SPACE]:
             if self.timer <= 0:
                 self.shoot()
